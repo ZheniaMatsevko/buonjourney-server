55d095d8dcff0f2dfe2b242d5cacf7fca0609e47
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2024-11-17 18:59:44 +0200

    fixed event and trip mappers

--- src/main/java/org/naukma/buonjourneyserver/mapper/IEventMapper.java
+++ src/main/java/org/naukma/buonjourneyserver/mapper/IEventMapper.java
@@ -1,8 +1,10 @@
 package org.naukma.buonjourneyserver.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.naukma.buonjourneyserver.dto.EventDto;
+import org.naukma.buonjourneyserver.dto.createDto.EventCreateDto;
 import org.naukma.buonjourneyserver.entity.EventEntity;
 
 @Mapper(componentModel = "spring")
@@ -10,4 +12,6 @@ public interface IEventMapper {
     IEventMapper INSTANCE = Mappers.getMapper(IEventMapper.class);
     EventDto entityToDto(EventEntity eventEntity);
     EventEntity dtoToEntity(EventDto eventDto);
+    @Mapping(target = "id", ignore = true)
+    EventEntity createDtoToEntity(EventCreateDto createDto);
 }
--- src/main/java/org/naukma/buonjourneyserver/mapper/ITripMapper.java
+++ src/main/java/org/naukma/buonjourneyserver/mapper/ITripMapper.java
@@ -1,8 +1,10 @@
 package org.naukma.buonjourneyserver.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.naukma.buonjourneyserver.dto.TripDto;
+import org.naukma.buonjourneyserver.dto.createDto.TripCreateDto;
 import org.naukma.buonjourneyserver.entity.TripEntity;
 
 @Mapper(componentModel = "spring")
@@ -10,4 +12,6 @@ public interface ITripMapper {
     ITripMapper INSTANCE = Mappers.getMapper(ITripMapper.class);
     TripDto entityToDto(TripEntity eventEntity);
     TripEntity dtoToEntity(TripDto eventDto);
+    @Mapping(target = "id", ignore = true)
+    TripEntity createDtoToEntity(TripCreateDto createDto);
 }
