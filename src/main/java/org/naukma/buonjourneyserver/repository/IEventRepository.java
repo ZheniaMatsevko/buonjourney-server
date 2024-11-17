3d832581fa5222ec2ebb50f3a1c752f781cf7d51
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2024-11-17 18:59:02 +0200

    fixed event and trip repositories

--- src/main/java/org/naukma/buonjourneyserver/repository/IEventRepository.java
+++ src/main/java/org/naukma/buonjourneyserver/repository/IEventRepository.java
@@ -4,6 +4,9 @@ import org.naukma.buonjourneyserver.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IEventRepository extends JpaRepository<EventEntity, Long> {
+    List<EventEntity> findAllByTripId(Long tripId);
 }
--- src/main/java/org/naukma/buonjourneyserver/repository/ITripRepository.java
+++ src/main/java/org/naukma/buonjourneyserver/repository/ITripRepository.java
@@ -4,6 +4,9 @@ import org.naukma.buonjourneyserver.entity.TripEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ITripRepository  extends JpaRepository<TripEntity, Long> {
+    List<TripEntity> findAllByUserId(Long userId);
 }
